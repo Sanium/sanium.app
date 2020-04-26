a5999d2bd7df00fd9c0b55c18734ffd6f52eb4a9
ReQezeR <mp62639@o2.pl>
2020-04-26 22:47:37 +0200

    add theme change button

--- lib/tools/drawer.dart
+++ lib/tools/drawer.dart
@@ -1,85 +1,172 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:sanium_app/themes/theme_options.dart';
+import 'package:sanium_app/tools/CustomScrollBehavior.dart';
+import 'package:theme_provider/theme_provider.dart';
 
 
-Widget mainDrawer(BuildContext context, Function onMap, Function onInfo, Function onBookmark) {
-  return Container(
+class MainDrawer extends StatelessWidget{
+  final Function onMap;
+  final Function onInfo;
+  final Function onBookmark;
+
+  MainDrawer({this.onMap, this.onInfo, this.onBookmark});
+
+  void changeTheme(bool value, BuildContext context)async{
+    ThemeProvider.controllerOf(context).nextTheme();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color iconColor = ThemeProvider.optionsOf<CustomThemeOptions>(context).defaultIconColor;
+    Color textColor = ThemeProvider.optionsOf<CustomThemeOptions>(context).mainTextColor;
+    Color cardColor = Theme.of(context).brightness == Brightness.light?Theme.of(context).primaryColor:ThemeProvider.optionsOf<CustomThemeOptions>(context).surfaceColor;
+    // isSwitched = Theme.of(context).brightness == Brightness.light?false:true;
+
+    return Container(
       width: MediaQuery.of(context).size.width * 0.65,
       child: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Image.asset('assets/sanium.png'),
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                border: Border(bottom: BorderSide(width: 3.0, color: Theme.of(context).dividerColor)),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: AutoSizeText(
-                  'Mapa',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: 'Open Sans',
-                    color: Theme.of(context).primaryColorDark,
+        child: Container(
+          color: Theme.of(context).brightness == Brightness.light?ThemeProvider.optionsOf<CustomThemeOptions>(context).backgroundColor:Theme.of(context).primaryColor,
+          child: ScrollConfiguration(
+            behavior: RemoveOverscrollIndicator(),
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.25,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 10, 5, 0),
+                      child: Theme.of(context).brightness == Brightness.light?Image.asset('assets/sanium.png'):Image.asset('assets/sanium_dark.png'),
+                    ),
+                    decoration: BoxDecoration(
+                      gradient: Theme.of(context).brightness == Brightness.light?LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [Colors.grey[500], cardColor, cardColor, Colors.grey[400]],
+                        stops: [
+                          0,
+                          0.4,
+                          0.7,
+                          1
+                        ],
+                      ):LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [Theme.of(context).primaryColor, cardColor, cardColor, Theme.of(context).primaryColor],
+                        stops: [
+                          0,
+                          0.4,
+                          0.7,
+                          1
+                        ],
+                      ),
+                      border: Border(bottom: BorderSide(width: 0.0, color: Theme.of(context).primaryColor)),
+                    ),
                   ),
-                  maxLines: 1,
                 ),
-                leading: Icon(Icons.map, color: Theme.of(context).primaryColorDark),
-                onTap: () {
-                  Navigator.pop(context);
-                  onMap();
-                }
-                  
-              ),
-            ),
-
-            Card(
-              child: ListTile(
-                title: AutoSizeText(
-                  'Zakładki',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: 'Open Sans',
-                    color: Theme.of(context).primaryColorDark,
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                  child: Card(
+                    color: cardColor,
+                    child: ListTile(
+                      title: AutoSizeText(
+                        'Mapa',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'Open Sans',
+                          color: textColor,
+                        ),
+                        maxLines: 1,
+                      ),
+                      leading: Icon(Icons.location_on, color: Theme.of(context).brightness == Brightness.light?iconColor:Colors.redAccent),
+                      onTap: () {
+                        Navigator.pop(context);
+                        onMap();
+                      }
+                        
+                    ),
                   ),
-                  maxLines: 1,
                 ),
-                leading: Icon(Icons.bookmark_border, color: Theme.of(context).primaryColorDark),
-                onTap: () {
-                  Navigator.pop(context);
-                  onBookmark();
-                }
-                  
-              ),
-            ),
 
-            Card(
-              child: ListTile(
-                title: AutoSizeText(
-                  'Informacje',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: 'Open Sans',
-                    color: Theme.of(context).primaryColorDark,
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                  child: Card(
+                    color: cardColor,
+                    child: ListTile(
+                      title: AutoSizeText(
+                        'Zakładki',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'Open Sans',
+                          color: textColor,
+                        ),
+                        maxLines: 1,
+                      ),
+                      leading: Icon(Icons.collections_bookmark, color: Theme.of(context).brightness == Brightness.light?iconColor:Colors.lightGreenAccent),
+                      onTap: () {
+                        Navigator.pop(context);
+                        onBookmark();
+                      }
+                    ),
                   ),
-                  maxLines: 1,
                 ),
-                leading: Icon(Icons.info_outline, color: Theme.of(context).primaryColorDark),
-                onTap: () {
-                  Navigator.pop(context);
-                  onInfo();
-                },
-              ),
-            ),
 
-          ],
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                  child: Card(
+                    color: cardColor,
+                    child: ListTile(
+                      title: AutoSizeText(
+                        'Tryb ${Theme.of(context).brightness == Brightness.light?'Jasny':'Ciemny'}',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'Open Sans',
+                          color: textColor,
+                        ),
+                        maxLines: 1,
+                      ),
+                      onTap: ThemeProvider.controllerOf(context).nextTheme,
+                      leading: Container(
+                        child: Theme.of(context).brightness == Brightness.light?Icon(Icons.wb_sunny, color:iconColor):Icon(Icons.brightness_3, color:Theme.of(context).accentColor),
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                  child: Card(
+                    color: cardColor,
+                    child: ListTile(
+                      title: AutoSizeText(
+                        'Informacje',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'Open Sans',
+                          color: textColor,
+                        ),
+                        maxLines: 1,
+                      ),
+                      leading: Icon(Icons.info_outline, color: Theme.of(context).brightness == Brightness.light?iconColor:Colors.blue),
+                      onTap: () {
+                        Navigator.pop(context);
+                        onInfo();
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
+}
